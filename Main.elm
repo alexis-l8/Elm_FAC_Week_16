fe3634e0e122678e0128a3110a146b28d15a8488
Alexis Lui <alexislui@hotmail.co.uk>
2017-06-07 15:20:52 +0100

    Ability to create intruction items and delete them individually or altogether relates #4 relates #5

--- Main.elm
+++ Main.elm
@@ -2,7 +2,87 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 main =
-    div [] [ text "Hello!" ]
+    Html.beginnerProgram
+        { view = view
+        , update = update
+        , model = model
+        }
+
+
+
+-- model
+
+
+type alias Model =
+    { todo : String
+    , todos : List String
+    }
+
+
+model : Model
+model =
+    { todo = ""
+    , todos = []
+    }
+
+
+
+-- update
+
+
+type Msg
+    = UpdateTodo String
+    | AddTodo
+    | RemoveAll
+    | RemoveItem String
+    | ClearInput
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateTodo text ->
+            { model | todo = text }
+
+        AddTodo ->
+            { model | todos = model.todo :: model.todos, todo = "" }
+
+        RemoveAll ->
+            { model | todos = [] }
+
+        RemoveItem text ->
+            { model | todos = List.filter (\element -> element /= text) model.todos }
+
+        ClearInput ->
+            { model | todo = "" }
+
+
+
+-- view
+
+
+todoItem : String -> Html Msg
+todoItem todo =
+    li [] [ text todo, button [ onClick (RemoveItem todo) ] [ text "x" ] ]
+
+
+todoList : List String -> Html Msg
+todoList todos =
+    let
+        child =
+            List.map todoItem todos
+    in
+    ul [] child
+
+
+view model =
+    div []
+        [ input [ type_ "text", onInput UpdateTodo, value model.todo ] []
+        , button [ onClick AddTodo ] [ text "Submit" ]
+        , button [ onClick RemoveAll ] [ text "Remove All" ]
+        , div [] [ todoList model.todos ]
+        ]
